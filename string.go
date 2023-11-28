1e3af0d72787dbe195932c636a4b6d14260e096e
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2023-11-28 20:22:42 +0700

    Study Go Day 1

---
+++ string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(len("Ryan")) //Hitung Karakter
+	fmt.Println("Muhammad Febriyan"[0]) // Akaan dapat Byte m = 77
+	fmt.Println("Muhammad Febriyan Akbar")
+}
---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var name = "Febriyan"
+	fmt.Println(name)
+
+	name = "Ryan"
+	fmt.Println(name)
+}
