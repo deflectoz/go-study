026a8239bfbaacfbe3e7f344dff5c465f79217f5
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2023-11-28 20:28:18 +0700

    [Update] Study Go Day 1

--- variable.go
+++ variable.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main(){
 
-	var name = "Febriyan"
+	name := "Febriyan" //Declare Pertama tipe data, string, bool etc
 	fmt.Println(name)
 
 	name = "Ryan"
 	fmt.Println(name)
+
+	var (
+		firstName = "Febriyan"
+		middleName = "Akbar"
+		lastName = "Dz"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
 }
